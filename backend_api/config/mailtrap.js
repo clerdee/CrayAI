76928966768f20ffa7859235c0df1da7702c6ea1
clerdee <clerdeecruz@gmail.com>
2026-02-26 16:07:31 +0800

    Fix: requirements

--- backend_api/config/mailtrap.js
+++ backend_api/config/mailtrap.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT), 
+  port: Number(process.env.MAIL_PORT) || 2525,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
