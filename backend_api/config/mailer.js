d314618aea7d6a08bf4a467187fd9cab2dcf54b8
clerdee <clerdeecruz@gmail.com>
2026-03-10 17:08:48 +0800

    bypass cloud server firewall rules

--- backend_api/config/mailer.js
+++ backend_api/config/mailer.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false 
   }
 });
 
