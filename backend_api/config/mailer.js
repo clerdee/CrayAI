9a9484d3a36b6f75e020957db9960b158bc8ea1e
clerdee <clerdeecruz@gmail.com>
2026-03-10 17:40:32 +0800

    google oauth2

--- backend_api/config/mailer.js
+++ backend_api/config/mailer.js
@@ -2,23 +2,21 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false 
+    clientId: process.env.MAIL_CLIENT_ID,
+    clientSecret: process.env.MAIL_CLIENT_SECRET,
+    refreshToken: process.env.MAIL_REFRESH_TOKEN
   }
 });
 
 transporter.verify(function(error, success) {
   if (error) {
-    console.error("SMTP error:", error);
+    console.error("OAuth2 error:", error);
   } else {
-    console.log("Real Gmail Mailer is ready to go!");
+    console.log("Real Gmail Mailer (OAuth2) is ready to go!");
   }
 });
 
