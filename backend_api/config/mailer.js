5241da0b8b89f47fb90142e453da06827746a171
clerdee <clerdeecruz@gmail.com>
2026-03-10 17:50:36 +0800

    googleapis

--- backend_api/config/mailer.js
+++ backend_api/config/mailer.js
@@ -1,23 +1,58 @@
-const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USER,
-    clientId: process.env.MAIL_CLIENT_ID,
-    clientSecret: process.env.MAIL_CLIENT_SECRET,
-    refreshToken: process.env.MAIL_REFRESH_TOKEN
-  }
-});
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.MAIL_CLIENT_ID,
+  process.env.MAIL_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
 
-transporter.verify(function(error, success) {
-  if (error) {
-    console.error("OAuth2 error:", error);
-  } else {
-    console.log("Real Gmail Mailer (OAuth2) is ready to go!");
+oAuth2Client.setCredentials({ refresh_token: process.env.MAIL_REFRESH_TOKEN });
+
+const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+const transporter = {
+  sendMail: async function(mailOptions) {
+    try {
+      const utf8Subject = `=?utf-8?B?${Buffer.from(mailOptions.subject).toString('base64')}?=`;
+      const messageParts = [
+        `From: ${mailOptions.from}`,
+        `To: ${mailOptions.to}`,
+        `Content-Type: text/html; charset=utf-8`,
+        `MIME-Version: 1.0`,
+        `Subject: ${utf8Subject}`,
+        '',
+        mailOptions.html || mailOptions.text
+      ];
+      
+      const message = messageParts.join('\n');
+
+      const encodedMessage = Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+
+      const res = await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: {
+          raw: encodedMessage,
+        },
+      });
+      
+      console.log("Email sent successfully via Gmail API!");
+      return res;
+      
+    } catch (error) {
+      console.error("Gmail API Error:", error);
+      throw error;
+    }
+  },
+  
+  verify: function(callback) {
+    console.log("Custom Gmail HTTPS Mailer is ready to go!");
+    if(callback) callback(null, true);
   }
-});
+};
 
 module.exports = transporter;
