efbe524ee24583817a29356aeccaecd930396409
clerdee <clerdeecruz@gmail.com>
2026-03-11 23:42:07 +0800

    chat routes

--- backend_api/routes/chatRoutes.js
+++ backend_api/routes/chatRoutes.js
@@ -9,5 +9,7 @@ router.post('/send', auth, chatController.sendMessage);
 router.get('/messages/:partnerId', auth, chatController.getMessages);
 router.post('/start', auth, chatController.startChat);
 router.post('/accept', auth, chatController.acceptChatRequest);
+router.delete('/message/:partnerId/:messageId', auth, chatController.deleteMessage);
+router.delete('/conversation/:partnerId', auth, chatController.deleteConversation);
 
 module.exports = router;
